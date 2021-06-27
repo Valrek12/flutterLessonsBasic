b284481af715ff2c9bd267c4bef014687422369b
vchernousov <vchernousov@logrocon.com>
2021-06-27 17:06:00 +0300

    добавил логику изменения цвета контейнера

--- flutter_application_basic/lib/pages/home_page.dart
+++ flutter_application_basic/lib/pages/home_page.dart
@@ -8,6 +8,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final ButtonStyle style =
+      ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+  Color color = Colors.blue;
+  int counter = 1;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,10 +27,25 @@ class _HomePageState extends State<HomePage> {
             width: 150,
             height: 150,
             decoration: BoxDecoration(
-                color: Colors.deepPurpleAccent,
+                color: color,
                 border: Border.all(color: Colors.black, width: 5.0),
                 borderRadius: BorderRadius.all(Radius.circular(20.0))),
-          )
+          ),
+          Center(
+            child: ElevatedButton(
+                style: style,
+                onPressed: () {
+                  setState(() {
+                    counter++;
+                    if (counter % 2 == 0) {
+                      color = Colors.amberAccent;
+                    } else {
+                      color = Colors.blue;
+                    }
+                  });
+                },
+                child: const Text('Поменять цвет')),
+          ),
         ],
       ),
     );
